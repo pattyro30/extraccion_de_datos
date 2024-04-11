fc628f6baf5ee6d8f10e6b017545efd8baf6567c
pattyro30 <a01709306@tec.mx>
2024-04-10 20:11:31 -0600

    Practicas de facturación del 1 al 7

--- practica_facturación.py
+++ practica_facturación.py
@@ -4,5 +4,36 @@ df = pd.read_excel("datos_facturacion.xlsx")
 print(df.head())
 
 filtro1 = df[ (df["CVE_CLPV"] >= 1000) & (df["CVE_CLPV"] <= 2000) ]
-print(filtro1)
+#print(filtro1)
 filtro1.to_csv("practica_facturacion_1.csv")
+
+filtro2 = df[~(df["CVE_VEND"] == 5.0 ) & ~(df["CVE_VEND"] == 4.0)]
+#print(filtro2)
+filtro2.to_csv("practica_facturacion2.csv")
+
+#filtro3 = df[ df["FECHA_ENT"].dt.strftime('%Y-%m-%d') == '2019-10-02']
+#filtro3 = df[ df["FECHA_ENT"].dt.strftime('%Y') == '2019']
+df['FECHAELAB'] = pd.to_datetime(df['FECHAELAB'])
+filtro3=df[ df["FECHAELAB"].dt.strftime('%Y-%m-%d') == '2019-10-02']
+#print(filtro3)
+filtro3.to_csv("practica_facturacion3.csv")
+
+filtro4=df[(df["CAN_TOT"] < 5951.7)| (df["STATUS"] == "E")]
+#print(filtro4)
+filtro4.to_csv("practica_facturacion4.csv")
+
+filtro5 = df.iloc[ : , [0, 6, 7,9]]  
+#print(filtro5)
+filtro5.to_csv("practica_facturacion5.csv")
+
+filtro6 = df.iloc[7001:7099, :  ] 
+#print(filtro6)
+filtro6.to_csv("practica_facturacion6.csv")
+
+
+df1= pd.read_excel('datos_facturacion.xlsx', index_col=3)
+df1.head()
+
+filtro7=df1.loc[[1.0,2.0], ["FECHAELAB"]]
+#print(filtro7)
+filtro7.to_csv("practica_facturacion7.csv")
